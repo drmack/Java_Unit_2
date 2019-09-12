46313ba11613fd721eede8c6e76cb0124f7d2120
SageMoon4702 <33138040+SageMoon4702@users.noreply.github.com>
2019-09-12 13:40:42 -0400

    9-2-19 U2L1 uploaded
    Fixed and uploaded lesson 1 of second unit

---
+++ Lesson 1 - Java Methods/LineBreak.java
@@ -0,0 +1,21 @@
+public class LineBreak
+{
+    public static void main(String[] args) {
+        run();
+    }
+    
+    public static void run()
+    {
+        System.out.println("Part 1");
+        printLineBreak();
+        
+        System.out.println("Part 2");
+        printLineBreak();
+    }
+    
+    private static void printLineBreak()
+    {
+        System.out.println("==========");
+        System.out.println("**********");
+    }
+}
